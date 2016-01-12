e9da8f66d90b7e731e0146f7ca17857d41015842
narendrathapa <thapa.narendra@gmail.com>
2016-01-12 12:08:28 -0500

    Initial Commit

---
+++ ContactList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ContactList
+//
+//  Created by Narendra Thapa on 2016-01-12.
+//  Copyright © 2016 Narendra Thapa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
